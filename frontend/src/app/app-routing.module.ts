08d19f373d48e0a09ac4928b0a1162d4e8a80a36
souhail-ouabou <souhail7ouabou@gmail.com>
2022-04-05 04:19:09 +0000

    connect angular tp spring boot

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CollabsComponent } from './components/collabs/collabs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CollabsComponent
+}, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
